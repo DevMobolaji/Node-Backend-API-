577734718be1047fc7d7bf498418f0a2c4988e7c
TechProff <alan08037896270@outlook.com>
2023-01-04 00:46:59 +0100

    added all file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = 'mongodb+srv://TechProf:Makanaki_12345@beejaycodes.iwhuw.mongodb.net/?retryWrites=true&w=majority'
+console.log(MONGO_URI)
 
 mongoose.connection.once("open", () => {
     console.log("MongDB connection ready!");
