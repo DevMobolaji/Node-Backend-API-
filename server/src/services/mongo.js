5ae37c0958321cc8a13958c1140b0e77c0a59b98
TechProff <alan08037896270@outlook.com>
2023-01-04 00:56:31 +0100

    Loaded planet data into actions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGO_URI = 'mongodb+srv://TechProf:Makanaki_12345@beejaycodes.iwhuw.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URI = process.env.MONGO_URI
 console.log(MONGO_URI)
 
 mongoose.connection.once("open", () => {
